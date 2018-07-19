2e9bef4ae01cefcbe46ab6886b6f1c0ca3746a82
itkindaworks <itkindaworksinc@gmail.com>
2018-07-18 21:39:53 -0400

    lib tweak/added macro for 3.3V systems

--- WaveshareSharpDustSensor.h
+++ WaveshareSharpDustSensor.h
@@ -11,20 +11,22 @@
 
 #define        COV_RATIO                       0.2            //ug/mmm / mv
 #define        NO_DUST_VOLTAGE                 400            //mv
-#define        SYS_VOLTAGE                     5000           
+
+
+// #define        SYS_VOLTAGE                     5000 		//5V
+#define        SYS_VOLTAGE                     3300			//3.3V
 class WaveshareSharpDustSensor{
 public:
-	WaveshareSharpDustSensor::WaveshareSharpDustSensor();
+	WaveshareSharpDustSensor();
 	float density;
 	float voltage;
 	int Filter(int m);
 	/// Convert raw ADC voltage reading (0-1023) into dust density in ug/m^3
 	float Conversion(int rawVoltageADC);
-	
+
 };
 
 
 
 
 #endif
-
